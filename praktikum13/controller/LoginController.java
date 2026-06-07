40d84f13cb0329cc8da5540762763f522835fd89
Luckyta Rizqia Jubaedi <luckytakyta142@gmail.com>
2026-06-07 21:51:05 +0700

    Add files via upload

---
+++ praktikum13/controller/LoginController.java
@@ -0,0 +1,15 @@
+package controller;
+
+import model.UserModel;
+
+public class LoginController {
+
+    /**
+     * Memproses login.
+     *
+     * @return UserModel jika berhasil, null jika gagal.
+     */
+    public UserModel prosesLogin(String username, String password) {
+        return UserModel.cariUser(username, password);
+    }
+}
